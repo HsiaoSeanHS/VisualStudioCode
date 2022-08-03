e837160af35ed35fa0868ff66f322489817a8e5f
HsiaoSeanHS <hsiaoseanhs@gmail.com>
2022-08-03 22:43:47 +0800

    0803

---
+++ javascript/如何使用數字、數字用法.html/script.js
@@ -0,0 +1,34 @@
+
+/*得餘數*/
+document.write(4%3);
+document.write("<br/>");
+
+/*取絕對值*/
+document.write(Math.abs(-1.23));
+document.write("<br/>");
+
+/*取最大值*/
+let a=1, b=-9, c=0.005;
+document.write(Math.max(a,b,c,0));
+document.write("<br/>");
+/*取最小值*/
+document.write(Math.min(a,b,c,0));
+document.write("<br/>");
+/*取四捨五入*/
+document.write(Math.round(c));
+document.write("<br/>");
+
+/*得指數*/
+document.write(Math.pow(b,0)); //Math.pow(底數,次方)
+document.write("<br/>");
+/*得平方根*/
+document.write(Math.sqrt(b)); //負數取平方根得NaN
+document.write("<br/>");
+document.write(Math.sqrt(16384));
+document.write("<br/>");
+
+/*得0到1之間的隨機數(為小數)*/
+document.write(Math.random());
+document.write("<br/>");
+
+/*2022.08.03*/
