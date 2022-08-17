dbbc2afa110e8eb4f4fa1289ec234eb26d310b0c
HsiaoSeanHS <hsiaoseanhs@gmail.com>
2022-08-17 19:07:40 +0800

    0817

---
+++ javascript/for迴圈.html/script.js
@@ -0,0 +1,10 @@
+
+let Rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violate"];
+
+for(let n = 0; n < Rainbow.length; n++){
+    document.write(Rainbow[n]);
+    document.write(".");
+    document.write("<br/>");
+}
+
+/*2022.08.17*/
