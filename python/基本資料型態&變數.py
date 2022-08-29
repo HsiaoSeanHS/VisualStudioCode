186089aa3ac84fb707653652f1f040c1a80463a7
HsiaoSeanHS <hsiaoseanhs@gmail.com>
2022-08-29 22:47:40 +0800

    0829

---
+++ python/基本資料型態&變數.py
@@ -0,0 +1,25 @@
+
+#基本資料型態
+#字串
+"窩不知道" #單引號也可以''，但是推薦使用雙引號""
+#數字
+666
+0.1234
+-9
+#Boolean
+True
+False
+
+#變數
+#命名規則: 1.只接受英文字母與數字、底線_的組合 2.不可以數字為開頭
+Whatever = "窩不知道"
+print(Whatever)
+Whatever = 87564231
+print(Whatever)
+Whatever = True
+print(Whatever)
+#變數類型不固定，可以自由轉換
+
+#清空下方終端機內容的指令為cls (Mac的指令為clear)
+
+#2022.08.29
