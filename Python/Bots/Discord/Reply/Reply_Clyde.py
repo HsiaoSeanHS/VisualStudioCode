afee711c376bff2a8cc29f85d3d1b4bfe5edffe4
HsiaoSeanHS <hsiaoseanhs@gmail.com>
2023-08-25 17:55:10 +0800

    Duplicate PA.txt

---
+++ Python/Bots/Discord/Reply/Reply_Clyde.py
@@ -0,0 +1,90 @@
+import json
+import discord
+from hugchat import hugchat
+from hugchat.login import Login
+
+def AcPw(Service):
+    PAs = open("D:/PA.txt", "r")
+    lines = PAs.readlines()
+    PAs.close()
+
+    for line in lines:
+        line = line.replace("\n", "")
+        if line.find("Email") == 0:
+            email = line.split(" ")[1]
+            print(email)
+        if line.find(Service) == 0:
+            passwd = line.split(" ")[1]
+            print(passwd)
+            break
+    return email, passwd
+
+# intents = discord.Intents.default()
+intents = discord.Intents.all()
+intents.messages = True
+client = discord.Client(intents = intents)
+to = "MTE0MzQxMDE4NTkxNDgyMjY5Nw "
+k = "Gs5BON "
+en = "GP_bgGZLlAgzUzx5RzGQOFsl82nNFzmCeEc9EY"
+T = to + k + en
+T = T.replace(" ", ".")
+
+# target_user_id = 598368596258717718 #1081004946872352958 Clyde
+target_channel_id = 1143553654293540864
+
+try:
+    cookie_json = open("D:/Cookies/hsiaoseanhs@gmail.com.json")
+    chatbot = hugchat.ChatBot(cookies = json.load(cookie_json))
+except:
+    email, passwd = AcPw("Huggingface")
+    sign = Login(email, passwd)
+    cookie = sign.login()
+    sign.saveCookiesToDir("D:/Cookies")
+    chatbot = hugchat.ChatBot(cookies = cookie.get_dict())
+
+
+
+async def HugCheat(id, content):
+    chatbot.change_conversation(id)
+    if content == "exit": 
+        # conversation_list = chatbot.get_conversation_list()
+        for id in chatbot.get_conversation_list(): chatbot.delete_conversation(id)
+        return None
+    return chatbot.chat(content)
+    
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name} ({client.user.id})')
+
+@client.event
+async def on_message(message):
+    try:
+        if message.channel.id == target_channel_id:
+            # if message.author.id == target_user_id:
+            if message.author.id != 1143410185914822697:
+                content = message.content
+                try: 
+                    if first == True: 
+                        first = False
+                        id = chatbot.new_conversation()
+                except: 
+                    first = True
+                    id = chatbot.new_conversation()
+                
+                if content == "exit": 
+                    await HugCheat(id, "exit")
+                    await message.channel.send("||  ||")
+                    first = True
+                else:
+                    reply_to = await message.channel.fetch_message(message.id)
+                    await reply_to.reply(await HugCheat(id, content))
+                print(id)
+    except Exception as e:
+        await message.channel.send("I'm broken.")
+        print(e)
+        try: 
+            for id in chatbot.get_conversation_list(): chatbot.delete_conversation(id)
+        except: pass
+
+client.run(T)
---
+++ Python/Bots/Discord/Reply/test.py
@@ -0,0 +1,156 @@
+# import requests
+
+# # Set your custom API endpoint URL here
+# custom_api_endpoint = "https://free.churchless.tech/v1/chat/completions"
+
+# def get_bot_response(user_input):
+#     payload = {
+#         "prompt": f"You: {user_input}\nBot:",
+#         "max_tokens": 50,  # Adjust the response length as needed
+#     }
+#     try:
+#         response = requests.post(custom_api_endpoint, json=payload)
+#         response_data = response.json()
+#         print("Response:", response_data)  # Print the full response for debugging
+#         bot_response = response_data.get("text", "").strip()
+#     except Exception as e:
+#         print("Error:", e)
+#         bot_response = "An error occurred while processing the request."
+#     return bot_response
+
+# # Main interaction loop
+# while True:
+#     user_input = input("You: ")
+#     if user_input.lower() == "exit":
+#         print("Bot: Goodbye!")
+#         break
+#     bot_response = get_bot_response(user_input)
+#     print(f"Bot: {bot_response}")
+
+# Not responding
+
+# from hugchat import hugchat
+# from hugchat.login import Login
+
+# email = "hsiaoseanhs@gmail.com"
+# passwd = "zAq!2WsE3$"
+
+# # 登入huggingface授权huggingchat
+# sign = Login(email, passwd)
+# cookies = sign.login()
+
+# # 保存cookies至 usercookies/<email>.json
+# sign.saveCookiesToDir("./Discord/Reply/Cookies")
+
+# # 创建一个 ChatBot
+# chatbot = hugchat.ChatBot(cookies=cookies.get_dict())  # or cookie_path="usercookies/<email>.json"
+
+# # 创建一个新会话
+# id = chatbot.new_conversation()
+
+
+# while True:
+#     chatbot.change_conversation(id)
+#     urin = input("You: ")
+#     if urin == "exit": break
+#     print("Bot: " + chatbot.chat(urin))
+#     conversation_list = chatbot.get_conversation_list()
+#     print(conversation_list)
+
+# for id in conversation_list: chatbot.delete_conversation(id)
+
+# print("Done.")
+
+# 获取对话列表
+# conversation_list = chatbot.get_conversation_list()
+
+# Only one time reply
+
+# import os, time
+
+# from hugchat_api import HuggingChat
+# from hugchat_api.core import ListBots
+# from hugchat_api.utils import formatHistory, formatConversations
+
+# # EMAIL = os.getenv("hsiaoseanhs@gmail.com")
+# # PASSWD = os.getenv("zAq!2WsE3$")
+# # COOKIE_STORE_PATH = "./usercookies"
+
+# '''create ThreadPool'''
+# HUG = HuggingChat(max_thread=1)
+
+
+# '''initialize sign in funciton'''
+# # sign = HUG.getSign(EMAIL, PASSWD)
+
+# '''sign in or...'''
+# # cookies = sign.login(save=True, cookie_dir_path=COOKIE_STORE_PATH)
+# # cookies = sign.loadCookiesFromDir()
+
+
+
+# '''create bot with MetaAI's model'''
+# bot = HUG.getBot(email=email, cookies=cookies, model=ListBots.META_70B_HF)
+
+# '''get all conversations and see one's title'''
+# conversations = bot.getConversations()
+# conv_id = list(conversations.keys())[0]
+# # print(conversations[conv_id])
+
+# '''get all chat histories by conversation_id'''
+# histories = bot.getHistoriesByID(conversation_id=conv_id)
+# # print(formatHistory(histories))
+
+# '''delete a conversation'''
+# bot.removeConversation(conversation_id=conv_id)
+
+# '''create a new conversation'''
+# conversation_id = bot.createConversation()
+
+# '''chat'''
+# while True:
+#     user_input = input("You: ")
+#     message = bot.chat(
+#         text=user_input,
+#         conversation_id=conversation_id,
+#         web_search=True,
+#         max_tries=2,
+#         # callback=(bot.updateTitle, (conversation_id,))
+#     )
+
+
+
+#     '''wait the full text or...'''
+#     while not message.web_search_done:
+#         time.sleep(0.1)
+#     # print(message.getWebSearchSteps())
+#     while not message.isDone():
+#         time.sleep(0.1)
+#     print("Bot: " + message.getFinalText())
+
+#     '''get the stream text instantly'''
+#     # print(message.getWebSearchSteps())
+#     # print(message.getText())
+
+PAs = open("D:/PA.txt", "r")
+lines = PAs.readlines()
+PAs.close()
+
+for line in lines:
+    line = line.replace("\n", "")
+    if line.find("Email") == 0:
+        email = line.split(" ")[1]
+        print(email)
+    if line.find("Huggingface") == 0:
+        passwd = line.split(" ")[1]
+        print(passwd)
+        break
+
+# import sys
+# sys.path.insert(0, '../')
+
+# from PA import PW
+
+# Email, Password = PW("HuggingFace")
+# print(Email)
+# print(Password)
